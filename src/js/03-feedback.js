0848a2375fd32285bc0d496c7bf60f58ae370239
Tetiana <tanya200291@ukr.net>
2023-05-03 12:44:46 +0200

    finash

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,27 +6,34 @@ const textContentInput = document.querySelector('input')
 
 textContentInput.addEventListener('input', throttle(onInput), 500);
 submitForm.addEventListener('submit', onSubmit);
-let saveMessage = JSON.parse(localStorage.getItem(LOCAL_KEY)) ;
+let saveMessage = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {} ;
 
 const {email, message} = submitForm.elements;
 textContent();
 
 
 function onInput(e){
-const inputText = { email: email.value, message: message.value};
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(inputText));
+ saveMessage = { email: email.value, message: message.value};
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(saveMessage));
     
 }
 function textContent(){
     if (saveMessage){
-        email.value = saveMessage.email ;
-        message.value = saveMessage.message ;
+        email.value = saveMessage.email || '';
+        message.value = saveMessage.message || '';
     }
 }
-function onSubmit(e){
+// Меседж зберігається тільки при вводі email
+function onSubmit(e) {
     e.preventDefault();
-    e.currentTarget.reset();
+    console.log({ email: email.value, message: message.value });
+  
+    if (email.value === '' || message.value === '') {
+      return alert('Please fill in all the fields!');
+    }
+  
     localStorage.removeItem(LOCAL_KEY);
-   saveMessage = {}
-}
+    e.currentTarget.reset();
+    saveMessage = {};
+  }
 
