4ee4bbeb5fe699793772400624eab0001e62eeaf
tetianakhyzhniak <118192553+tetianakhyzhniak@users.noreply.github.com>
2023-05-03 06:28:39 +0000

    Deploying to gh-pages from @ tetianakhyzhniak/goit-js-hw-08@b301a2a4c1cbc56d128705f236c5590e109a1e97 🚀

---
+++ 03-feedback.429f4a99.js
@@ -0,0 +1,2 @@
+!function(){LOCAL_KEY="feedback-form-state";var e=document.querySelector(".feedback-form");document.querySelector("input").addEventListener("input",(function(e){var t={email:r.value,message:n.value};localStorage.setItem(LOCAL_KEY,JSON.stringify(t))})),e.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset(),localStorage.removeItem(LOCAL_KEY),t={}}));var t=JSON.parse(localStorage.getItem(LOCAL_KEY)),a=e.elements,r=a.email,n=a.message;t&&(r.value=t.email,n.value=t.message)}();
+//# sourceMappingURL=03-feedback.429f4a99.js.map
