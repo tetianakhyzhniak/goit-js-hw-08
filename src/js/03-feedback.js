b301a2a4c1cbc56d128705f236c5590e109a1e97
Tetiana <tanya200291@ukr.net>
2023-05-03 08:28:11 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
 
+LOCAL_KEY = "feedback-form-state"
+const submitForm = document.querySelector('.feedback-form');
+const textContentInput = document.querySelector('input')
+
+textContentInput.addEventListener('input', onInput);
+submitForm.addEventListener('submit', onSubmit);
+let saveMessage = JSON.parse(localStorage.getItem(LOCAL_KEY)) ;
+
+const {email, message} = submitForm.elements;
+textContent();
+
+
+function onInput(e){
+const inputText = { email: email.value, message: message.value};
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(inputText));
+    
+}
+function textContent(){
+    if (saveMessage){
+        email.value = saveMessage.email ;
+        message.value = saveMessage.message ;
+    }
+}
+function onSubmit(e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LOCAL_KEY);
+   saveMessage = {}
+}
+
